c37d3f66ede6cf597446093697fd8f0c3ef81def
Kevin <manoz@outlook.com>
2014-09-07 15:34:08 +0200

    Better Gulp organisation

--- gulpfile.js
+++ gulpfile.js
@@ -33,16 +33,20 @@ var AUTOPREFIXER = [
 
 /**
  * CSS Task
+ * @todo Move minify and .rename in min:css task
  */
 gulp.task('dist:css', function () {
+
     return gulp.src('src/scss/main.scss')
         .pipe(plug.plumber())
         .pipe(plug.sass({
-            outputStyle: 'nested'   // 'nested' or 'compressed'
+            outputStyle: 'compressed'   // 'nested' or 'compressed'
         }))
         .pipe(plug.autoprefixer(AUTOPREFIXER))
+        .pipe(plug.rename({ suffix: '.min' }))
         .pipe(gulp.dest('dist/css'))
         .pipe(plug.size({ title: 'dist:css' }))
+
 });
 
 
@@ -51,12 +55,8 @@ gulp.task('dist:css', function () {
  */
 gulp.task('dist:js', function() {
 
-    return gulp.src('src/js/main.js')
+    return gulp.src('src/js/*.js')
         .pipe(plug.plumber())
-        .pipe(plug.jshint())
-        .pipe(plug.jshint.reporter(plug.stylish))
-        .pipe(plug.uglify())
-        .pipe(plug.rename({ suffix: '.min' }))
         .pipe(gulp.dest('dist/js'))
         .pipe(plug.size({ title: 'dist:js' }))
 
@@ -67,11 +67,47 @@ gulp.task('dist', ['dist:css', 'dist:js']);
 
 
 /**
+ * Minify and concat tasks
+ */
+
+// JS
+gulp.task('min:js', function () {
+
+    return gulp.src('src/js/main.js')
+        .pipe(plug.uglify())
+        .pipe(plug.rename({ suffix: '.min' }))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(plug.size({ title: 'dist:min:js' }))
+
+});
+
+// Run our JS and CSS minify tasks
+gulp.task('min', ['min:js']);
+
+
+/**
+ * Lint JS
+ */
+gulp.task('lint:js', function () {
+
+    return gulp.src('src/js/*.js')
+        .pipe(plug.plumber())
+        .pipe(plug.jshint())
+        .pipe(plug.jshint.reporter(plug.stylish))
+        //.pipe(plug.jshint.reporter('fail'))
+
+});
+
+// Run the lint task
+gulp.task('lint', ['lint:js']);
+
+
+/**
  * Create our default task and watch for changes
  */
-gulp.task('default', ['dist'], function() {
+gulp.task('default', ['dist', 'lint', 'min'], function() {
 
     gulp.watch('src/scss/**/*.scss', ['dist:css']);
-    gulp.watch('src/js/**/*.js', ['dist:js']);
+    gulp.watch('src/js/**/*.js', ['lint:js', 'dist:js']);
 
 });
